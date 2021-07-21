bc66fb5903c7b191ef386eec26d81f23d8046334
Jeongin205 <81507170+Jeongin205@users.noreply.github.com>
2021-07-22 00:10:11 +0900

    회원가입 xml완성

--- app/src/main/java/org/techtown/vacationproject337/LoginActivity.java
+++ app/src/main/java/org/techtown/vacationproject337/LoginActivity.java
@@ -2,13 +2,25 @@ package org.techtown.vacationproject337;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
-
+    Button join;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        join = findViewById(R.id.btn_join);
+        join.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, JoinActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
