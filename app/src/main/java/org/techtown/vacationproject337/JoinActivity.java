aa8f442628fff99a60b7b9ab3f8f6c96ba302377
Jeongin205 <81507170+Jeongin205@users.noreply.github.com>
2021-07-26 03:59:14 +0900

    확인하고 빨간

--- app/src/main/java/org/techtown/vacationproject337/JoinActivity.java
+++ app/src/main/java/org/techtown/vacationproject337/JoinActivity.java
@@ -5,9 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -16,10 +19,13 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.regex.Pattern;
+
 public class JoinActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     EditText mName, mEmail, mPwd, mPwdch;
     Button join;
+    TextView tv_error_email, tv_error_pwd, tv_error_pwdch;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +34,10 @@ public class JoinActivity extends AppCompatActivity {
         final DatabaseReference table_user = database.getReference("User");
         mAuth = FirebaseAuth.getInstance();
 
+        tv_error_email = findViewById(R.id.tv_error_email);
+        tv_error_pwd = findViewById(R.id.tv_error_pwd);
+        tv_error_pwdch = findViewById(R.id.tv_error_pwdch);
+
         mName = findViewById(R.id.et_Name);
         mEmail = findViewById(R.id.et_User_email);
         mPwd = findViewById(R.id.et_User_pwd);
@@ -35,14 +45,65 @@ public class JoinActivity extends AppCompatActivity {
 
         join = findViewById(R.id.btn_jjoin);
 
+        mEmail.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(!android.util.Patterns.EMAIL_ADDRESS.matcher(s.toString()).matches()){
+                    tv_error_email.setText("이메일 형식으로 입력해주세요.");    // 경고 메세지
+                    mEmail.setBackgroundResource(R.drawable.red_edittext);  // 적색 테두리 적용
+                }
+                else{
+                    tv_error_email.setText("");         //에러 메세지 제거
+                    mEmail.setBackgroundResource(R.drawable.edit);  //테투리 흰색으로 변경
+                }
+            }// afterTextChanged()..
+        });
+
+        mPwd.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(!Pattern.matches("^(?=.*[a-zA-Z0-9])(?=.*[a-zA-Z!@#$%^&*])(?=.*[0-9!@#$%^&*]).{8,15}$")
+                {
+                    tv_error_pwd.setText("숫자, 문자, 특수문자중 2가지를 포함하세요");
+                    mPwd.setBackgroundResource(R.drawable.red_edittext);
+                }
+                else{
+                    tv_error_pwd.setText("");
+                    mPwd.setBackgroundResource(R.drawable.edit);
+                }
+            }
+        });
+
         join.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String name = mName.getText().toString();
+                String email = mEmail.getText().toString();
+                String pwd = mPwd.getText().toString();
+                String pwdch = mPwdch.getText().toString();
+
                 UserAccount account = new UserAccount();
-                final ProgressDialog mDialog = new ProgressDialog(JoinActivity.this);
-                mDialog.setMessage("기다려주세요");
-                mDialog.show();
-                account.uid = mAuth.getUid();
+                //final ProgressDialog mDialog = new ProgressDialog(JoinActivity.this);
+                //mDialog.setMessage("기다려주세요");
+                //mDialog.show();
+
+
 
             }
         });
