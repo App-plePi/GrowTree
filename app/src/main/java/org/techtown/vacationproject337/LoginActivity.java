1679cdbb188ab38a494b634be20c4e68911b16d1
Jeongin205 <81507170+Jeongin205@users.noreply.github.com>
2021-07-28 01:45:25 +0900

    로그인 기능 구현

--- app/src/main/java/org/techtown/vacationproject337/LoginActivity.java
+++ app/src/main/java/org/techtown/vacationproject337/LoginActivity.java
@@ -1,26 +1,85 @@
 package org.techtown.vacationproject337;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Paint;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.techtown.vacationproject337.databinding.ActivityJoinBinding;
+import org.techtown.vacationproject337.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
-    Button join;
+    private ActivityLoginBinding binding;
+    FirebaseAuth firebaseAuth;
+    boolean isEmail = false;
+    boolean isPwd = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        join = findViewById(R.id.btn_join);
-        join.setOnClickListener(new View.OnClickListener() {
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        binding.etEmail.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onClick(View v) {
+            public void beforeTextChanged(CharSequence s, int start, int count, int after){}
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(s!=null) isEmail = true;
+                else isEmail = false;
+            }
+        });
+        binding.etPwd.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(s!=null) isPwd = true;
+                else  isPwd =true;
+            }
+        });
+
+        binding.btnJoin.setOnClickListener(v -> {
                 Intent intent = new Intent(LoginActivity.this, JoinActivity.class);
                 startActivity(intent);
+        });
+        binding.btnLogin.setOnClickListener(v -> {
+            if(isEmail&&isPwd){
+                String email = binding.etEmail.getText().toString();
+                String pwd = binding.etPwd.getText().toString();
+                firebaseAuth.signInWithEmailAndPassword(email, pwd).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Intent intent =new Intent(LoginActivity.this, MainActivity2.class);
+                            startActivity(intent);
+                        }
+                        else Toast.makeText(getApplicationContext(), "로그인오류", Toast.LENGTH_SHORT).show();
+                        //어디서 틀려서 로그인 안돼는지 알려주기
+                    }
+                });
             }
+            else {
+                if(!isEmail&&!isPwd) Toast.makeText(getApplicationContext(), "정보를 입력해주세요", Toast.LENGTH_SHORT).show();
+                else if(!isEmail)Toast.makeText(getApplicationContext(), "이메일을 입력해주세요", Toast.LENGTH_SHORT).show();
+                else if(!isPwd)Toast.makeText(getApplicationContext(), "비밀번호를 입력해주세요", Toast.LENGTH_SHORT).show();
+        }
         });
     }
 }
