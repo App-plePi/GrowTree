25769259d0f5df65f783be85e94f3830b5da9d0c
kiyoji0003 <81694402+kiyoji0003@users.noreply.github.com>
2021-08-09 21:22:14 +0900

    .

--- app/src/main/java/org/techtown/vacationproject337/Fragment/HomeFragment.kt
+++ app/src/main/java/org/techtown/vacationproject337/Fragment/HomeFragment.kt
@@ -1,23 +1,37 @@
-package org.techtown.vacationproject337.Fragment;
+package org.techtown.vacationproject337.Fragment
 
-import android.os.Bundle;
+import android.os.Bundle
+import android.renderscript.ScriptGroup
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.core.view.isGone
+import androidx.core.view.isInvisible
+import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
+import org.techtown.vacationproject337.R
+import org.techtown.vacationproject337.databinding.FragmentHomeBinding
+import java.util.*
+import kotlin.concurrent.timer
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
+class HomeFragment : Fragment() {
+    private var time = 0
+    private var timerTask : Timer? = null
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
-import org.techtown.vacationproject337.R;
+        val binding = FragmentHomeBinding.inflate(inflater,container,false)
 
-public class HomeFragment extends Fragment {
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v=inflater.inflate(R.layout.fragment_home,container,false);
-        return v;
+        binding.startBtnMain.setOnClickListener{
+            binding.startBtnMain.visibility = View.GONE
+            binding.endBtnMain.visibility = View.VISIBLE
+        }
+        binding.endBtnMain.setOnClickListener {
+            binding.startBtnMain.visibility = View.VISIBLE
+            binding.endBtnMain.visibility = View.GONE
+        }
+        return binding.root
 
     }
 }
